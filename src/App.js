8b237f4818e7f75bd9d01b2dcaaa6979f1e96fd6
Poppy Curtis <poppy.curtis@gmail.com>
2019-01-07 15:31:57 +0000

    Add your favourite movie cover #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moviePicture from './nextgen.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Hello World 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={moviePicture} alt='nextgen picture'/>
       </div>
     );
   }
