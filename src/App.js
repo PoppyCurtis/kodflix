0a88731b649bcb11122fc32be37de6e23a3c9533
Poppy Curtis <poppy.curtis@gmail.com>
2019-01-08 14:54:04 +0000

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import bigHero6 from './images/bighero.jpg';
+import got from './images/got.jpg';
+import suits from './images/suits.jpg';
+import ointb from './images/ointb.jpg';
+import fairyTale from './images/fairytale.jpg';
+import insideOut from './images/insideOut.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div className="container">
-        <div className="item">Big Hero 6</div>
-        <div className="item">Game of Thrones</div>
-        <div className="item">Orange is the New Black</div>         
+          <div className="container">
+        <div className="item">
+          <img src={suits} alt='Picture of Suits'/>
+        </div>
+        <div className="item">
+          <img src={got} alt='Picture of Game of Thrones'/>
+        </div>
+        <div className="item">
+          <img src={insideOut} alt='Picture of Inside Out'/>
+        </div>         
       </div>
       <div className="container">
-        <div className="item">Suits</div>
-        <div className="item">Fairy Tail</div>
-        <div className="item">Inside Out</div>         
+        <div className="item">
+          <img src={bigHero6} alt='Picture of Big Hero 6'/>
+        </div>
+        <div className="item">
+          <img src={fairyTale} alt='Picture of Fairy Tail'/>
+        </div>
+        <div className="item">
+          <img src={ointb} alt='Picture of Orange is the New Black'/>
+        </div>         
       </div>
       </div>
     );
