ab5948e561776e8719dd69e18661b0cc508173d9
Poppy Curtis <poppy.curtis@gmail.com>
2019-01-28 16:18:35 +0000

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const tvshow = require('./tvshowslist');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/rest/shows', (req, res) => {
+    res.send(tvshow());
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/backend/tvshowslist.js
@@ -0,0 +1,12 @@
+function tvshow()  {
+    return  [
+        { "id": "suits", "name": "Suits",  "details": "In Suits, one of Manhattan's top corporate lawyers (Gabriel Macht) sets out to recruit a new hotshot associate and hires the only guy that impresses him, a brilliant but unmotivated college dropout (Patrick J. Adams). Though he isn't actually a lawyer, this legal prodigy has the book smarts of a Harvard law grad and the street smarts of a hustler. However, in order to serve justice and save their jobs, both these unconventional thinkers must continue the charade."},
+        { "id":"got", "name": "Game of Thrones",  "details": "Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years."},
+        { "id":"insideout", "name": "Inside Out", "details": "After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school." },
+        {"id": "bighero", "name": "Big Hero 6",  "details": "The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes." },
+        {"id": "fairytail", "name": "Fairy Tail",  "details": "Lucy, an aspiring Celestial Wizard, becomes a friend and ally to powerful wizards Natsu, Gray, and Erza, who are part of the (in)famous wizard guild, Fairy Tail." },
+        { "id":"ointb", "name": "Orange Is The New Black", "details": "Convicted of a decade old crime of transporting drug money to an ex-girlfriend, normally law-abiding Piper Chapman is sentenced to a year and a half behind bars to face the reality of how life-changing prison can really be." }
+    ];
+}
+
+module.exports = tvshow;
