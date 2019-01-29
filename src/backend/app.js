ebf006733fec198f98b9b8e52dd2dcff990c00ca
Poppy Curtis <poppy.curtis@gmail.com>
2019-01-29 15:41:04 +0000

    Run frontend and backend servers in parallel #21

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const tvshow = require('./tvshowslist');
+const port = 3001;
+const getTvShowList = require('./tv-show-list');
 
 app.get('/rest/shows', (req, res) => {
-    res.send(tvshow());
+    res.send(getTvShowList());
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
