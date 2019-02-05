9bab19b843969bb4b344e219c6f937a281822839
Poppy Curtis <poppy.curtis@gmail.com>
2019-02-05 12:32:31 +0000

    backend server ammendments

---
+++ src/srcFontEnd/Gallery.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TvShow from './TvShow.js';
+import getImageGallery from './getGalleryImages';
+
+export default function Gallery() {
+    return (
+        <div>
+            <div className="container">
+                {
+                    getImageGallery().map(tvShow => (
+                        <TvShow
+                            key={tvShow.id}
+                            id={tvShow.id}
+                            name={tvShow.name}
+                            logo={tvShow.logo} />
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
