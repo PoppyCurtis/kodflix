b958f5899a097939e2b45c0d7bc2be30981f329a
Poppy Curtis <poppy.curtis@gmail.com>
2019-02-27 12:02:16 +0000

    Add a loading indicator to let the user know 'something is going on' until content is available #25

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import TvShow from './TvShow';
+import Loader from './Loading/Loading'
+import './Loading/Loading.css';
 
 
 export default class Gallery extends Component {
     constructor() {
         super()
         this.state = {
-           movies: []
+            movies: []
         }
     }
 
 
     componentDidMount() {
         fetch('/rest/shows')
-            .then( response => {
+            .then(response => {
                 return response.json();
             })
             .then(movies => {
@@ -26,16 +28,17 @@ export default class Gallery extends Component {
             <div>
                 <div className="container">
                     {
-                        this.state.movies.map(movie => (
-                            <TvShow id={movie.id} name={movie.name} />
-                        ))
-                    }
+                        this.state.movies.length ?
+                            this.state.movies.map(movie => (
+                                <TvShow id={movie.id} name={movie.name} />
+                            ))
+                            : <div className="container">
+                                <Loader />
+                            </div>}
                 </div>
             </div>
         );
+
     }
 }
-
-
-
 
