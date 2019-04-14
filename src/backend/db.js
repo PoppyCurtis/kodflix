65196cbd02d1c937267d52a6f4b9e4c8856f35b2
Poppy Curtis <poppy.curtis@gmail.com>
2019-04-14 21:15:12 +0100

    #29 Create a remote database

--- src/backend/db.js
+++ src/backend/db.js
@@ -2,9 +2,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
- const url = 'mongodb://localhost:27017/';
-
-
+const url = 'mongodb://localhost:27017/';
+ 
  function connect() {
     return new Promise((resolve, reject) => {
         MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
