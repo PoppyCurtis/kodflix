591bc6bce1e508419584a55e5636ac95150b5299
Poppy Curtis <poppy.curtis@gmail.com>
2020-01-27 22:29:20 +0000

    Point to PRD database# 30

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,14 +1,18 @@
-
+require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017/';
- 
+const url = process.env.DB_URL_PRD;
+
+const index = url.lastIndexOf("kodflix-copy");
+const database = url.substr(index, 12);
+const fullURL = url + process.env.DB_URL_PRD_PARAMS + '&w=majority&collection=kodflix-copy';
+
  function connect() {
     return new Promise((resolve, reject) => {
-        MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        MongoClient.connect(fullURL, { useUnifiedTopology: true }, { useNewUrlParser: true },  function (err, db) {
             assert.equal(null, err);
-            const dbo = db.db('kodflix');
+            const dbo = db.db(database);
             resolve(dbo);
         });
     });
