6fbd0dcb09aaea61ed7cf64698e43545e39ff078
Poppy Curtis <poppy.curtis@gmail.com>
2020-02-02 12:14:26 +0000

    Add support for customer analytics! #31

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Gallery from './Gallery.js';
 import Details from './Details/details.js';
 import NotFound from './NotFound.js';
+import ReactGA from 'react-ga';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    ReactGA.initialize('UA-157101100-1');
+    ReactGA.pageview(window.location.pathname);
+    
     return (
-      <Router>
-        <div className="Gallery">
-          <Switch>
-            <Route exact path='/' component={Gallery} />
-            <Route exact path='/not-found' component={NotFound} />
-            <Route exact path='/:tvShowId' component={Details} />
-          </Switch>
-        </div>
-      </Router>
+
+      <div className="Gallery">
+        <Switch>
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/not-found' component={NotFound} />
+          <Route exact path='/:tvShowId' component={Details} />
+        </Switch>
+      </div>
+
     );
   }
 }
--- src/frontend/TvShow.js
+++ src/frontend/TvShow.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Gallery from './Gallery';
 
 export default function TvShow(properties) {
   return (
