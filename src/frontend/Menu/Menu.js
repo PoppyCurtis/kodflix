98eb3cc914743acc9f9af21be827e143779c9d89
Poppy Curtis <poppy.curtis@gmail.com>
2020-02-02 20:24:48 +0000

    Create the vertical menu panel #34

--- src/frontend/Menu/Menu.js
+++ src/frontend/Menu/Menu.js
@@ -3,17 +3,38 @@ import './Menu.css';
 
 export default class Gallery extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            menuIsVisible: false
+        };
+    }
+
     buttonClick = event => {
-        console.log('clicked!')
+
+        if (this.state.menuIsVisible === false) {
+            this.setState({ menuIsVisible: true });
+        }
+        else {
+            this.setState({ menuIsVisible: false });
+        }
     }
 
     render() {
+
         return (
-            <button id="menu" onClick={this.buttonClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24">
-                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 17h-12v-2h12v2zm0-4h-12v-2h12v2zm0-4h-12v-2h12v2z" />
-                </svg>
-            </button>
+            <div id='menuContainer'>
+                <button id="menu" onClick={this.buttonClick}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 17h-12v-2h12v2zm0-4h-12v-2h12v2zm0-4h-12v-2h12v2z" />
+                    </svg>
+                </button>
+                <div className={(this.state.menuIsVisible === true ? 'is-visible' : 'not-visible')}>
+
+                    Hello Menu
+            </div>
+            </div>
         );
     }
 }
